f272e88850821d10e01eba8fd9e98da9adefb0c3
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2021-11-19 16:21:02 -0600

    psuedo code added

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 
+// Create function for generatePassword because its being called in the writePassword function
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria 
+
+//Prompt the user for password criteria 
+// Password length 8 characters and no more than 128 characters
+// Lowercase, Uppercase, Numbers and Special Characters 
+
+// Validate Input
+
+// Validate Criteria met
+
+// Display Password on Page - RETURN
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
